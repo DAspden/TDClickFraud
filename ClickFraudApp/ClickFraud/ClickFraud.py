c1b3dffb1c3055e0120812fd7503b582873425f3
Denny <dendenlehman@gmail.com>
2019-01-15 23:22:57 -0800

    added to summary data

--- ClickFraudApp/ClickFraud/ClickFraud.py
+++ ClickFraudApp/ClickFraud/ClickFraud.py
@@ -2,18 +2,25 @@
 import os
 import pandas as pd
 import matplotlib
+import seaboarn as sns
 
-class Dataloader(object):
+class Dataloader():
 	"""docstring for Dataloader"""
-	def __init__(self, train_data_file_name, test_data_file_name):
+	def __init__(self, train_data_file_name, test_data_file_name, nrows=None):
 		# create path
-		app_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-		# print(app_path)
-		if not train_data_file_name.endswith('.csv'): train_data_file_name += '.csv'
-		self.train_data = os.path.join(app_path,'data',train_data_file_name)
-		if not test_data_file_name.endswith('.csv'): test_data_file_name += '.csv'
-		self.test_data = os.path.join(app_path,'data',test_data_file_name)
-
+		path = 1
+		if path == 0:
+			pass
+			# app_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+			# # print(app_path)
+			# if not train_data_file_name.endswith('.csv'): train_data_file_name += '.csv'
+			# self.train_data = os.path.join(app_path,'data',train_data_file_name)
+			# if not test_data_file_name.endswith('.csv'): test_data_file_name += '.csv'
+			# self.test_data = os.path.join(app_path,'data',test_data_file_name)
+		else:
+			self.train_data = train_data_file_name
+			self.test_data = test_data_file_name
+		self.df = self.load_data(nrows)
 
 
 	def __repr__(self):
@@ -30,20 +37,29 @@ is_attributed: not included'''
 	# nrows is an optional argument, it is initially none, which would have read_csv operate as if no argument was passed
 	def load_data(self,nrows=None):
 		self.df = pd.read_csv(self.train_data, nrows=nrows)
+		return self.df
+
 
 	def get_data_summary(self):
 		s = 'Summary of Data\n\n'
 		s += 'Index: \n' + str(self.df.index) + '\n\n'
 		s += 'Columns: \n' + str(self.df.columns) + '\n\n'
-		s += 'Description: \n' + str(self.df.describe())
-
-		self.df['app']
+		s += 'Description: \n' + str(self.df.describe()) + '\n\n'
+		s += 'Histogram: \n' + ' ' + '\n\n'
+		self.df.hist()
+		s += 'dan test! histograms should be above, but they wont be!\n\n' 
+		s += 'box plot time'
+		sns.boxplot(x=df['os'])
+		sns.boxplot(x=df['channel'])
+		df[df['os']<400].hist(column='os',bins=100)
+		self.df.boxplot()
 		return s
 
 	def test2(self):
 		self.load_data()
 		print(self.get_data_summary())
-		print(self.df.hist())
+		#print(self.df.hist())
+       
 	def test1(self):
 		print(self.train_data)
 		print(self.test_data)
